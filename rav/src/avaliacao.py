fe5a2704938b36e62d363a8d73e19cfaa9318193
bobonri0685 <bobonri@gmail.com>
2024-06-15 17:44:45 -0300

    reorganizacao das pastas

--- rav/src/avaliacao.py
+++
@@ -1,6 +0,0 @@
-# src/avaliacao.py
-
-def validate_evaluation_form(student_name, subject, grade):
-    if not student_name or not subject or not isinstance(grade, (int, float)) or grade < 0 or grade > 10:
-        return False
-    return True
--- rav/src/cadastro.py
+++
@@ -1,6 +0,0 @@
-# src/cadastro.py
-
-def validate_student_form(name, age, className):
-    if not name or not isinstance(age, int) or age <= 0 or not className:
-        return False
-    return True
